5536def73131d9361c45bd37f387cb9df7c87aa2
maeta187 <nexus18789@gmail.com>
2025-03-06 22:07:53 +0900

    🚚  : useActionStateルートの追加

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes'
 
 export default [
   index('routes/home.tsx'),
-  route('action/:id', 'routes/action/action.tsx')
+  route('action/:id', 'routes/action/action.tsx'),
+  route('useActionState', 'routes/useActionState/useActionState.tsx'),
 ] satisfies RouteConfig
