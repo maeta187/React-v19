4bbcb4c13664d750de9324eefe3a3a230a03ba3b
maeta187 <nexus18789@gmail.com>
2025-03-08 15:35:20 +0900

    🚚  : useActionStateルートの名前をuse-action-stateに変更

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes'
 export default [
   index('routes/home.tsx'),
   route('action/:id', 'routes/action/action.tsx'),
-  route('useActionState', 'routes/useActionState/useActionState.tsx'),
+  route('use-action-state', 'routes/useActionState/useActionState.tsx'),
   route('form-error', 'routes/useActionState/formError.tsx'),
   route(
     'display-structured-information',
