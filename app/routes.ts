190dc7b59db9a892227fca3d00538110452d54ea
maeta187 <nexus18789@gmail.com>
2025-03-08 14:45:11 +0900

    🚚  : form-errorおよびdisplay-structured-informationルートの追加

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,9 @@ export default [
   index('routes/home.tsx'),
   route('action/:id', 'routes/action/action.tsx'),
   route('useActionState', 'routes/useActionState/useActionState.tsx'),
+  route('form-error', 'routes/useActionState/formError.tsx'),
+  route(
+    'display-structured-information',
+    'routes/useActionState/displayStructuredInformation.tsx'
+  )
 ] satisfies RouteConfig
