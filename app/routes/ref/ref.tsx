8a7e18c30843aec5021912a9961850b842b9f266
maeta187 <nexus18789@gmail.com>
2025-03-10 23:12:43 +0900

    💬  : refのコメント修正

--- app/routes/ref/ref.tsx
+++ app/routes/ref/ref.tsx
@@ -10,6 +10,7 @@ type InputProps = {
  * React19以降親コンポーネントからrefをpropsで渡せるようになった
  * これにより、親コンポーネントから子コンポーネントのDOM要素にアクセスできるようになった
  * 型定義はReact.RefObjectし、<>の中でHTML要素の型と初期値を指定する
+ * 今まで方法だと forwardRef という関数を使っていたが、将来のバージョンで非推奨になる予定
  */
 function Input({ placeholder, ref }: InputProps) {
   return (
